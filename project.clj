982c95807962619ec18e4b0ce268628405ffafc2
Isaac Johnston <isaac@basecity.com>
2018-10-12 13:21:21 +0800

    Bump version for production release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject loco "0.3.1"
+(defproject flybot/loco "0.4.1"
   :description "Constraint Programming for Clojure"
-  :url "http://github.com/aengelberg/loco"
+  :url "http://github.com/flybot-sg/loco"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
